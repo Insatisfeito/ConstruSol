a4826d4a316e1c2cfb45d2b9fd57d74dda6e6e37
Daniel Carvalho <mail@danielcarvalho.pt>
2017-08-25 23:25:17 +0100

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'compare', to: 'construsolutions#compare'
   post 'compare', to: 'construsolutions#compareStep2'
   post 'compare-results', to: 'construsolutions#compareResults'
-
+  get 'compare-results', to: 'construsolutions#compare'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
