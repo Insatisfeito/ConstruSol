177875198f5d796b282ab4fe763f5b498447254f
Daniel Carvalho <mail@danielcarvalho.pt>
2017-09-18 07:30:35 +0100

    wip

--- app/assets/javascripts/src/app.js
+++ app/assets/javascripts/src/app.js
@@ -15,7 +15,8 @@ var app = function() {
 
 
     var buildDataTables = function() {
-        $('#example').dataTable();
+      
+      
         $("#panel1link").click(function() {
           $("#construsolution_type").val(0);
         });
@@ -23,7 +24,10 @@ var app = function() {
           $("#construsolution_type").val(1);
         });
         $(".chosen-select").chosen({no_results_text: "Sem resultados! ",allow_duplicates: true});
-        $(".chosen-select-compare").chosen({no_results_text: "Sem resultados! ",allow_duplicates: false ,max_selected_options: 3});      
+        $(".chosen-select-compare").chosen({no_results_text: "Sem resultados! ",allow_duplicates: false ,max_selected_options: 3});    
+        var dataTable = $('#example');
+        if(dataTable === null) {}
+        else{$('#example').dataTable();}  
     }
 
     var calculateResults = function() {
